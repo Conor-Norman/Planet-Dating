54c37dff9ded70f07c5527d7a6ca924553394e3b
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-11-24 12:43:50 -0500

    imported ink test
    started testing ink and created a test sceneario

---
+++ Game/Planet-Dating/Assets/Planet Dating/Ink Files/Ink Test.ink
@@ -0,0 +1,11 @@
+Cosmic Crush
+
+welcome to our game
+
+you are a bartender in a space lounge
+
+*sounds cool
+*why...?
+*i hate this!
+
+you beat the game
---
+++ Game/Planet-Dating/Assets/Planet Dating/Scripts/InkTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+
+public class InkTest : MonoBehaviour
+{
+    public TextAsset inkJSON;
+    public Story story;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+        story = new Story(inkJSON.text); //gets json file of scene
+
+        Debug.Log(LoadStoryChunk()); //shows all dialogue up until next choice
+
+        for (int i = 0; i < story.currentChoices.Count; i++) { //displaying all choice options
+            Debug.Log(story.currentChoices[i].text);
+        }
+
+        story.ChooseChoiceIndex(0); //manually picking first option
+
+        Debug.Log(LoadStoryChunk()); //displaying text after choice
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    string LoadStoryChunk() {
+
+        string text = "";
+
+        if (story.canContinue) {
+            text = story.ContinueMaximally(); //may want to remove maximally if player will click through
+        }
+        return text;
+    }
+}
