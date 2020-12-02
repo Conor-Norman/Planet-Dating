47a1d45a293cec0c2879689316e5d2bce7ae6ae7
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-12-01 22:33:33 -0500

    added in game text
    now showing text in game view
    started work on button choices
    all text can be clicked through until a choice is hit

--- Game/Planet-Dating/Assets/Planet Dating/Scripts/InkTest.cs
+++ Game/Planet-Dating/Assets/Planet Dating/Scripts/InkTest.cs
@@ -1,44 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Ink.Runtime;
 
 public class InkTest : MonoBehaviour
 {
+    #region Variables
+
+    [Header("ink JSON files")]
     public TextAsset inkJSON;
     public Story story;
+    public List<TextAsset> inkFiles = new List<TextAsset>();
+    public int sceneNumber;
+
+    [Header ("Canvas")]
+    public Text dialogueBox;
+    public Button choice1Button;
+    public Button choice2Button;
+    public Button choice3Button;
 
 
+    bool progressPressed = false;
+
+    #endregion
+
+    #region Unity Functions
     // Start is called before the first frame update
     void Start() {
 
-        story = new Story(inkJSON.text); //gets json file of scene
+        sceneNumber = 0; //start of game will be changed based on main menu selection
+
+        story = new Story(inkFiles[sceneNumber].text); //gets json file of scene
 
         story.EvaluateFunction("changeName", "Conor"); //detects and changes the players name
 
-        Debug.Log(LoadStoryChunk()); //shows all dialogue up until next choice
+        dialogueBox.enabled = true; //enables the text box
 
-        for (int i = 0; i < story.currentChoices.Count; i++) { //displaying all choice options
-            Debug.Log(story.currentChoices[i].text);
-        }
-
-        story.ChooseChoiceIndex(0); //manually picking first option
-
-        Debug.Log(LoadStoryChunk()); //displaying text after choice
+        LoadStory(); //shows first section of dialogue
     }
 
     // Update is called once per frame
     void Update() {
+
+        ButtonCheck();
         
     }
+    #endregion
 
-    string LoadStoryChunk() {
+    #region User Functions
+    void ButtonCheck() {
+        if ((Input.GetAxis("Progress") > 0 || Input.GetAxis("Progress") < 0) && !progressPressed) {
+            progressPressed = true;
+            dialogueBox.text = LoadStory(); //displaying text after choice
+        }
+        else if (Input.GetAxis("Progress") == 0) {
+            progressPressed = false;
+        }
+    }
+
+
+    string LoadStory() {
 
         string text = "";
 
         if (story.canContinue) {
-            text = story.ContinueMaximally(); //may want to remove maximally if player will click through
+            text = story.Continue(); //may want to remove maximally if player will click through
+        }
+        else {
+            //move to next scene or something by changing where the list is pointing
+            dialogueBox.enabled = false;
+            sceneNumber++;
         }
         return text;
     }
+
+
+    #endregion
 }
+
+/* for choices
+ * need to check when a choice is happening
+ * disable dialogue box
+ * check amount of choices there are
+ * enable each button for the amount
+ * put correct text for each button 
+ * after choice disable each button and chack which choice was made
+ * ensure correct path was gone down
+ * enable dialogue box
+ */
